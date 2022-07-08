b8a50802727db4e64c34e0d6284deee424ecec8c
elvisdorkenoo <elvisdorkenoo@gmail.com>
2022-07-08 09:59:51 +0000

    create targets

--- targets.js
+++ targets.js
@@ -1,1 +1,32 @@
-module.exports = [];
+module.exports = [
+
+  // % FOLLOW-UP CHILD HEALTH THIS MONTH
+  {
+    id: 'follow-up-child-health-rate-this-month',
+    type: 'percent',
+    icon: 'medic-health-facility',
+    goal: 80,
+    translation_key: 'Follow-up Child Health',
+    subtitle_translation_key: 'This month',
+    appliesTo: 'reports',
+    idType: 'report',
+    appliesIf: true,
+    passesIf: true,
+    date: 'now',
+  },
+
+  // CHILD ASSESSMENT THIS MONTH
+  {
+    id: 'child-assessment-this-month',
+    type: 'count',
+    icon: 'infant',
+    goal: -1,
+    translation_key: 'Child Assessment',
+    subtitle_translation_key: 'This month',
+    appliesTo: 'reports',
+    idType: 'report',
+    appliesIf: true,
+    date: 'reported',
+  },
+];
+
